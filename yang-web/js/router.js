45194b044b20aa4fe0d157987c8b69e92eceb398
Yang <125204642+Yangder201@users.noreply.github.com>
2024-10-24 16:59:03 +0800

    Add files via upload

---
+++ yang-web/js/router.js
@@ -0,0 +1,18 @@
+// 載入 header
+fetch('header.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('portfolio_menu').innerHTML = data;
+})
+.catch(error => console.error('Error loading header:', error));
+
+// 載入 footer
+fetch('footer.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('footer').innerHTML = data;
+})
+.catch(error => console.error('Error loading footer:', error));
+
+
+
